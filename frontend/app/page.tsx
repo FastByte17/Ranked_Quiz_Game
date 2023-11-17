33d27987cb283e7dcf9aa6feaaa69d9f2b8fea82
Nabeel_Hussain <nabeelbaig962@gmail.com>
2023-11-17 23:26:01 +0200

    researched on tailwindCSS, implemented some of it in the elements. bg still needs to be updated. added logic for checking if the user has played or not before. title is rendered dynamically. additional research on Next.JS

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,21 +2,33 @@
 
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRef } from 'react';
 
-export default function Home() {
+export default function Home({ score, setScore }) {
+  const { current } = useRef('ranked');
+  const hasPlayed = score > 0;
   return (
-    <div>
+    <div className="flex flex-col items-center gap-24 w-screen h-screen px-4 pt-[5%]">
       <div>
-        <div>
+        <div id="label">
+          {current.split('').map((letter, index) => (
+            <span
+              key={letter + index}
+              className="text-7xl capitalize font-Catamaran font-bold tracking-wider"
+            >
+              {letter}
+            </span>
+          ))}
 
         </div>
-        <h1>Guess the highest market value</h1>
+        <h1 className='text-xl font-Catamaran'>Guess the highest market value!</h1>
       </div>
-      <button type="button">Play!
-        <Link href={''}>
+      <button type="button">
+        <Link href={''} onClick={() => setScore(0)}>
+          {hasPlayed ? 'PLAY AGAIN' : 'PLAY'}
         </Link>
       </button>
-      <div>Total Points:</div>
+      {hasPlayed && (<div>Total Points: {score}</div>)}
     </div>
   )
 }
